ba1fe4580e6f743c15aaf3fcd451213d13de3d76
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-07-25 18:01:08 -0700

    Create basic components and add alan button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
+const alanKey =
+  '59e0561031e3254b162477ad3642bba72e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({command}) => {
+        if (command === 'testCommand') {
+          alert('This code was executed');
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Alan AI News Application</h1>
     </div>
   );
-}
+};
 
 export default App;
