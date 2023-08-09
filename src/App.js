9ed8cbc927d0b16de75d98412b8be4ed483bd54b
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-08-09 15:56:20 -0600

    Add some styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {useState, useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
 
 const alanKey =
   '59e0561031e3254b162477ad3642bba72e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     alanBtn({
@@ -23,7 +25,13 @@ const App = () => {
 
   return (
     <div>
-      <h1>Alan AI News Application</h1>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg"
+          className={classes.alanLogo}
+          alt="alan logo"
+        />
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   );
