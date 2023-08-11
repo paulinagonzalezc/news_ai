59ae8ba1b8e8fee83f163e20e7fa931a01b097b9
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-08-11 15:00:56 -0600

    Fix App.js path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './app';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
